3b79d1b961cd1de20d45ffe7c208728af69830a5
Tejas Giri <tejasgiri910@gmail.com>
2022-11-29 07:40:20 -0800

    updatd

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.get('/', (req, res) => {
     
 // })
 
-// app.get('/api/item/:slug', (req, res) => {
-//     const { slug } = req.params;
-//     res.end(`Item: ${slug}`);
-//   });
+app.get('/api', (req, res) => {
+    
+    res.send(`api called`);
+  });
 
 
 app.listen(port, (error)=>{
