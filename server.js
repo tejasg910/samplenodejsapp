69c0d21b7870e34bcbc423fb557a4a528f4e160d
Tejas Giri <tejasgiri910@gmail.com>
2022-11-29 07:21:17 -0800

    add

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app  = express();
+const { v4 } = require('uuid');
 const port = process.env.PORT || 5000
 app.get('/' , (req, res)=>{
-    res.status(200).json({success: true})
+    const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+    
 })
+
+app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+  });
 
 
 app.listen(port, (error)=>{
